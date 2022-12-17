206cac6e78ce4a524af78d68f9b667d01fa0a56a
Abundant Life International Ministries <120852095+alimdesk@users.noreply.github.com>
2022-12-17 17:36:07 -0600

    Add files via upload

---
+++ src/components/ThemeSettings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ThemeSettings(props) {
+
+  return (
+<div></div>
+  );
+}
+export default ThemeSettings;
---
+++ src/components/TimerSettings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TimerSettings(props) {
+
+  return (
+<div></div>
+  );
+}
+export default TimerSettings;
