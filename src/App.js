6289ac9194443bcf822e7082c38448b15c634627
Abundant Life International Ministries <120852095+alimdesk@users.noreply.github.com>
2022-12-29 17:16:57 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,29 +4,64 @@ import Settings from './components/Settings';
 import Screen from './components/Screen';
 import EditButton from './components/EditButton';
 
-function App() {
-  const timeToInt=() =>{
-    const dateobj = new Date();
-    let minhour = dateobj.getHours();
-    let minminutes = ((dateobj.getMinutes())<10? `0${dateobj.getMinutes()}`:`${dateobj.getMinutes()}`);
-    let mintime = `${minhour}${minminutes}`;
-    return parseInt(mintime);
-  }
+//make time update itself
+//create theme settings
+//create timer settings
+//fix all ui
+//create save to localstorage
+//add modifiable duration
+//create menu buttons
+//try to fix keydown pause
 
+
+function App() {
+  const fontarray = ["Helvetica, sans-serif",
+   "Arial, sans-serif",
+   "Arial Black, sans-serif",
+   "Verdana, sans-serif",
+   "Tahoma, sans-serif",
+   "Trebuchet MS, sans-serif",
+   "Impact, sans-serif",
+   "Gill Sans, sans-serif",
+   "Times New Roman, serif",
+   "Georgia, serif",
+   "Palatino, serif",
+   "Baskerville, serif",
+   "Andalé Mono, monospace",
+   "Courier, monospace",
+   "Lucida, monospace",
+   "Monaco, monospace",
+   "Bradley Hand, cursive",
+   "Brush Script MT, cursive",
+   "Comic Sans MS, cursive",
+   "Luminari, fantasy"
+  ]
+  const animarray=[
+    "Slide",
+    "Fade",
+    "Scroll"
+  ]
   const [pause, setPause] = useState(true);  
   const [inputText, setInputText] = useState("");
-  const [inputColor, setInputColor] = useState("#FFFFFF");
-  const [inputFont, setInputFont] = useState("helvetica , sans-serif");
-  const [inputSize, setInputSize] = useState(20);
+  const [inputColor, setInputColor] = useState("#000000");
+  const [inputFont, setInputFont] = useState(fontarray[0]);
+  const [inputSize, setInputSize] = useState(40);
   const [inputTheme, setInputTheme] = useState("");
+  const [inputAnim, setInputAnimation] = useState(animarray[0]);
   const [menuOptions, setMenuOptions] = useState("text");
-  const [currentTime, setCurrentTime] = useState(timeToInt());
+  const [crntScrRender, setcrntScrRender] = useState(0);
+  const [currentTime, setCurrentTime] = useState(Date.now());
   const [lines, setLines] = useState([]);
-  const [timer, setTimer] = useState({message: "",target: ""}); 
+  const [timer, setTimer] = useState({message: "",target: "11:00",color:"white",font: fontarray[0],size: 40,animate: animarray[0]}); 
+  const playOrPause =()=>{
+    let state = !pause;
+    setPause(state);
+  }
+
   return (
     <div className="App">
-      <EditButton setPause={setPause}/>
-      {pause===true?      <Settings
+      {pause===true?
+      <Settings
       setInputText={setInputText}
       setCurrentTime={setCurrentTime}
       setLines={setLines}
@@ -37,6 +72,9 @@ function App() {
       setInputSize={setInputSize}
       setInputTheme={setInputTheme}
       setMenuOptions={setMenuOptions}
+      setInputAnimation={setInputAnimation}
+      playOrPause={playOrPause}
+      inputAnim={inputAnim}
       inputTheme={inputTheme}
       inputFont={inputFont}
       inputSize={inputSize}
@@ -47,10 +85,15 @@ function App() {
       lines={lines}
       timer={timer}
       menuOptions={menuOptions}
-      />:      <Screen
+      fontarray={fontarray}
+      animarray={animarray}
+      />: <Screen
       lines={lines}
       timer={timer}
       currentTime={currentTime}
+      playOrPause={playOrPause}
+      crntScrRender={crntScrRender}
+      setcrntScrRender={setcrntScrRender}
       />}
     </div>
   );
