4ba0063ca8ca75b5cb9380c84d184b1a99bef016
Abundant Life International Ministries <120852095+alimdesk@users.noreply.github.com>
2023-01-08 06:22:47 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import Settings from './components/Settings';
 import Screen from './components/Screen';
 
-//buttons for listtext
 //fix all ui
 //try to fix keydown pause
 //add more fonts?
+//fix css for onn/off button
 
 
 function App() {
